2037bc7496596455156608743df5f24313831d22
BallisticEel <97997886+gilesshaxted@users.noreply.github.com>
2025-07-29 13:15:38 +0100

    Update shopDealDetector.js

--- events/shopDealDetector.js
+++ events/shopDealDetector.js
@@ -8,8 +8,8 @@ const TARGET_SHOP_BOT_ID = '1195112846976090322'; // User ID of the Lootcord Wor
 const ITEM_CATEGORIES = {
     RESOURCES: ['gunpowder', 'cloth', 'low grade fuel', 'high quality metal', 'metal spring'],
     LOOT: ['elite crate', 'metal armor', 'locked crate', 'large present', 'medium present', 'gold egg', 'egg basket', 'bronze egg', 'military crate', 'silver egg', 'supply drop', 'supply signal', 'small present', 'small loot bag', 'medium loot bag', 'large loot bag'],
-    WEAPONS: ['c4', 'bolt rifle', 'pistol bullet', 'f1 grenade', 'grenade launcher', 'l96', 'm249', 'mp5', 'spas-12', 'rocket launcher'],
-    AMMO: ['12g slug', `hv pistol bullet`, 'incen rifle bullet', 'incen pistol bullet', '40mm he grenade', 'rocket', 'explosive bullet', 'hv rifle bullet', 'hv rocket', 'rifle bullet', 'rocket'],
+    WEAPONS: ['c4', 'bolt rifle', 'f1 grenade', 'grenade launcher', 'l96', 'm249', 'mp5', 'spas-12', 'rocket launcher'],
+    AMMO: ['12g slug', `hv pistol bullet`, 'pistol bullet', 'incen rifle bullet', 'incen pistol bullet', '40mm he grenade', 'rocket', 'explosive bullet', 'hv rifle bullet', 'hv rocket', 'rifle bullet', 'rocket'],
     MEDS: ['medical syringe', 'large med kit']
 };
 
